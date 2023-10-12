078bb3da487a2638d5128338eea3034f2ebc9a07
Ryan Hedgecock <rhedgeco@gmail.com>
2023-10-11 20:35:47 -0700

    added auto shutdown to window example

--- examples/window.rs
+++ examples/window.rs
@@ -12,5 +12,13 @@ fn main() {
         .world
         .insert(WindowBuilder::<TaroRenderer>::new(WindowConfig::default()));
 
+    milk_tea
+        .world
+        .insert_callback::<MilkTeaUpdate>(|event, world| {
+            if world.len::<MilkTeaWindow<TaroRenderer>>() == 0 {
+                event.control_flow_mut().set_exit();
+            }
+        });
+
     milk_tea.run();
 }
