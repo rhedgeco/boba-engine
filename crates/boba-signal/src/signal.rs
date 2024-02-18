391b3be39c02626c35daf5de38a087ee957489cd
Ryan Hedgecock <rhedgeco@gmail.com>
2024-02-17 17:31:35 -0800

    remove unsafe code from signal

--- crates/boba-signal/src/signal.rs
+++ crates/boba-signal/src/signal.rs
@@ -1,3 +1,5 @@
+use std::rc::Rc;
+
 use boba_core::{
     world::{Link, PearlView, WorldQueue},
     Pearl, World,
@@ -6,52 +8,14 @@ use extension_trait::extension_trait;
 use indexmap::IndexSet;
 
 pub type Listener<P, T> = fn(PearlView<P>, &mut T);
+pub(crate) type Sender<T> = Rc<dyn Fn(&mut WorldQueue, &mut T)>;
 
-struct ListenerStore<T> {
-    link: Link<()>,
-    listener_ptr: *const (),
-    runner: fn(Link<()>, &mut WorldQueue, *const (), &mut T),
-}
-
-impl<T> Copy for ListenerStore<T> {}
-impl<T> Clone for ListenerStore<T> {
-    fn clone(&self) -> Self {
-        Self {
-            link: self.link.clone(),
-            listener_ptr: self.listener_ptr.clone(),
-            runner: self.runner.clone(),
-        }
-    }
-}
-
-impl<T> ListenerStore<T> {
-    pub fn new<P: Pearl>(link: Link<P>, listener: Listener<P, T>) -> Self {
-        Self {
-            link: link.into_type(),
-            // WARNING: dangerous type erasure goes on here to lift 'static restriction on T
-            listener_ptr: listener as *const (),
-            runner: |link, queue, listener_ptr, message| {
-                let link = link.into_type::<P>();
-                if let Some(view) = PearlView::new(link, queue) {
-                    // SAFETY: listener_ptr was created from an identical fn pointer
-                    let listener: Listener<P, T> = unsafe { core::mem::transmute(listener_ptr) };
-                    listener(view, message);
-                }
-            },
-        }
-    }
-
-    pub fn send_to(self, world: &mut WorldQueue, message: &mut T) {
-        (self.runner)(self.link, world, self.listener_ptr, message);
-    }
-}
-
-pub struct SignalBuilder<T> {
+pub struct SignalBuilder<T: 'static> {
     links: IndexSet<Link<()>>,
-    listeners: Vec<ListenerStore<T>>,
+    listeners: Vec<Sender<T>>,
 }
 
-impl<T> Default for SignalBuilder<T> {
+impl<T: 'static> Default for SignalBuilder<T> {
     fn default() -> Self {
         Self {
             links: IndexSet::new(),
@@ -60,7 +24,7 @@ impl<T> Default for SignalBuilder<T> {
     }
 }
 
-impl<T> SignalBuilder<T> {
+impl<T: 'static> SignalBuilder<T> {
     pub fn new() -> Self {
         Self::default()
     }
@@ -74,11 +38,17 @@ impl<T> SignalBuilder<T> {
 }
 
 #[extension_trait]
-pub impl<T> SignalRegister<T> for SignalBuilder<T> {
+pub impl<T: 'static> SignalRegister<T> for SignalBuilder<T> {
     fn add_listener<P: Pearl>(&mut self, link: Link<P>, listener: Listener<P, T>) {
         let (index, _) = self.links.insert_full(link.into_type());
-        self.listeners
-            .insert(index, ListenerStore::new(link, listener));
+        self.listeners.insert(
+            index,
+            Rc::new(move |world, data| {
+                if let Some(view) = PearlView::new(link, world) {
+                    listener(view, data);
+                }
+            }),
+        );
     }
 
     fn remove_listener<P: Pearl>(&mut self, link: &Link<P>) -> bool {
@@ -91,27 +61,27 @@ pub impl<T> SignalRegister<T> for SignalBuilder<T> {
     }
 }
 
-pub struct Signal<T> {
+pub struct Signal<T: 'static> {
     message: T,
-    listeners: Vec<ListenerStore<T>>,
+    listeners: Vec<Sender<T>>,
 }
 
-impl<T> Signal<T> {
+impl<T: 'static> Signal<T> {
     pub fn send(mut self, world: &mut WorldQueue) {
         for listener in self.listeners {
-            listener.send_to(world, &mut self.message);
+            listener(world, &mut self.message);
         }
     }
 }
 
 #[extension_trait]
-pub impl<T> WorldSignalExt<T> for WorldQueue<'_> {
+pub impl<T: 'static> WorldSignalExt<T> for WorldQueue<'_> {
     fn send_signal(&mut self, signal: Signal<T>) {
         signal.send(self)
     }
 }
 
-impl<T> WorldSignalExt<T> for World {
+impl<T: 'static> WorldSignalExt<T> for World {
     fn send_signal(&mut self, signal: Signal<T>) {
         let mut queue = WorldQueue::new(self);
         queue.send_signal(signal);
